60cda6dd50bfb33e83285e067b6289410b3edd18
Raymond Mojica <rm@M12020.local>
2022-07-11 23:10:25 -0400

    show route connected

---
+++ views/places/show.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+const Def = require('../default')
+
+
+function show(){
+    return(
+        <Def>
+            <main>
+                <h1>Show</h1>
+            </main>
+        </Def>
+    )
+
+}
+
+module.exports = show
