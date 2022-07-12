ecf2839fb71f49b943acc8524049a23d7416d7b2
Raymond Mojica <rm@M12020.local>
2022-07-12 00:11:12 -0400

    Delete function

--- controllers/places.js
+++ controllers/places.js
@@ -13,7 +13,7 @@ router.get('/:id', (req, res) => {
     else if (!places[id]){
         res.render('error404')
     }else{
-        res.render('places/show',{place:places[id]})
+        res.render('places/show',{place:places[id], id})
     }
 })
 
@@ -38,6 +38,19 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })
 
+router.delete('/places/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)){
+        res.render('error404')
+    }
+    else if (!places[id]){
+        res.render('error404')
+    }
+    else{
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
 /*router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
