bd46c9dfa4bafb4449ce3688dc394c881ded82ba
Raymond Mojica <rm@M12020.home>
2022-07-25 03:07:05 -0400

    Adjusted show routes

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,7 +8,25 @@ function show(data){
             No comments yet!
         </h3>
     )
+    let rating= (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if(data.place.comment.length){
+        let sumRatings = data.place.comments.reduce((tot, c) =>{
+            return tot + c.stars
+        },0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ' '
+        for( let i = 0; i < averageRating; i++){
+            stars += '⭐️'
+        }
+        rating = (
+            <h3>
+                {stars} Stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return(
                 <div className="border">
@@ -18,6 +36,9 @@ function show(data){
                         <strong>- {c.author}</strong>
                     </h3>
                     <h4>Rating : {c.stars}</h4>
+                    <form method="POST" action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}>
+                        <input type="submit" className="btn btn-danger" value="Delete Comment" />
+                    </form>
                 </div>
             )
         })
@@ -36,7 +57,9 @@ function show(data){
                 <h3>located in {data.place.city}, {data.place.state}</h3>
                 <h3>{data.place.showEstablished()}</h3>
                 <h4>Serving {data.place.cuisines}</h4>
-                <h2>Currently Unrated</h2>
+                <h2>Rating</h2>
+                {rating}
+                <br />
                 <h2>Comments</h2>
                 {comments}
                 <form method="POST" action={`/places/${data.id}/comment?_method=POST`}>
